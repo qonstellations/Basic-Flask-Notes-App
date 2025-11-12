f2332c27cf05e187b9a8603db8608a2ee14bc2df
Aarush <aarush.a.pathak@gmail.com>
2025-11-13 00:19:43 +0530

    added delete note functionality

--- website/auth.py
+++ website/auth.py
@@ -23,12 +23,12 @@ def login():
         else:
             flash("Email does not exist. Please Sign Up!", category="error")
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route("/logout")
 @login_required
 def logout():
-    logout_user(user)
+    logout_user()
     return redirect(url_for("auth.login"))
 
 @auth.route("/sign-up", methods=["GET", "POST"])
@@ -59,4 +59,4 @@ def sign_up():
             login_user(user, remember=True)
             return redirect(url_for("views.home"))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
